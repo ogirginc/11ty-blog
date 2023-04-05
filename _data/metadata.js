c6430285b22ff54f4d87838f172d5e83becd11e0
Oğulcan Girginç <ogulcan@girginc.com>
2023-04-05 21:28:29 +0300

    Update metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,10 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
-	"url": "https://example.com/",
+	"title": "Oğulcan Girginç's Blog",
+	"url": "https://ogirginc.com/",
 	"language": "en",
 	"description": "I am writing about my experiences as a naval navel-gazer.",
 	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
+		"name": "Oğulcan Girginç",
+		"url": "https://ogirginc.com/about-me/"
 	}
 }
